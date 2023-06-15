8e50c2ce29bbbae05676975e57b0a4dcee7f3821
Omer Ozceylan <omerozceylan35@gmail.com>
2023-06-15 04:22:00 +0300

    feat starting to write GroupSymbol component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const objs = [
+    {name:'google',
+     img:'../src/imgs/google.jpg'
+    },
+    {name:'rockstar',
+     img:'../src/imgs/rockstar.jpg'
+    },
+    {name:'midjourney',
+     img:'../src/imgs/mid.png'
+    }
+  ]
+
+
   return (
     <>
     <div className="discord">
       <div className='groups'>
-
+        <GroupSymbol imgName='rockstasdr' obj={objs}></GroupSymbol>
       </div>
       <div className="group">
         <div className="group-header">
@@ -41,6 +54,18 @@ function App() {
   )
 }
 
+function GroupSymbol({imgName, obj}) {
+  let imgSource = null
+  obj.map((value)=> {
+      if(value.name == imgName) {
+        imgSource = value.img
+        console.log(imgSource)
+      }
+  })
+  return(
+    <img src={imgSource} alt="" />
+  )
+}
 
 
 export default App
