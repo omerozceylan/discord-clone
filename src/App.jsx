dabb94a16e95e1fbed32d2377c9d2209d80b48f5
Omer Ozceylan <omerozceylan35@gmail.com>
2023-06-15 03:24:36 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className="discord">
+      <div className='groups'>
+
+      </div>
+      <div className="group">
+        <div className="group-header">
+
+        </div>
+        <div className="group-chats-section">
+        
+          <div className="group-chats-section__chats">
+
+          </div>
+          <div className="group-chats-section__user-section">
+
+          </div>
+        </div>
+        <div className="chat-section">
+        
+          <div className="chat-section__messages">
+
+          </div>
+          <div className="chat-section__input">
+
+          </div>
+        </div>
+        <div className="users-section">
+
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+
+
+export default App
